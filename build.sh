2f553036b89e170e18614d0e2caa08bacc62a2b3
yourb <98460564+yourbikun@users.noreply.github.com>
2024-12-09 15:22:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-cmake -B build -DCMAKE_INSTALL_PREFIX="./install" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake -B build -DCMAKE_INSTALL_PREFIX="./install" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 cmake --build build --config Release
+
 cmake --install build
